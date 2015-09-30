ceff623570071b8a658770d10a1a3c88f8449431
Daudt <rcdaudt@gmail.com>
2015-09-30 23:18:34 +0200

    Update

--- mod2result.py
+++ mod2result.py
@@ -10,6 +10,11 @@ import numpy as np
 def mod2result(U,bc):
     x = np.zeros(25)
     
+    # If necessary, initialize x[23] and x[24] to have different result,
+    # default values are both at 0
+    #x[23] = 1
+    #x[24] = 0
+    
     for i in range(22, -1, -1):
         temp = bc[i]
         for j in range(25):
