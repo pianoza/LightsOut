93084f6bf24d68bd02b986d07612e88ab0893bb5
Daudt <rcdaudt@gmail.com>
2015-10-06 00:15:14 +0200

    Ugly but working GUI

--- main.py
+++ main.py
@@ -7,24 +7,36 @@ Created on Sat Oct  3 13:54:28 2015
 
 from Tkinter import *
 
-#==============================================================================
-# def paintWhite(event):
-#     wideLabel.configure(background='white')
-# 
-# def paintBlack(event):
-#     wideLabel.configure(background='black')
-#==============================================================================
+def onPressedButton(i):
+    if isPlaying:
+        column = i % 5
+        row = (i - column)/5
+        if (row+1)<5:
+            lightGrid[(row+1)*5+column].toggle()
+        if (row-1)>-1:
+            lightGrid[(row-1)*5+column].toggle()
+        if (column+1)<5:
+            lightGrid[row*5+column+1].toggle()
+        if (column-1)>-1:
+            lightGrid[row*5+column-1].toggle()
+            
+def onPlayButton():
+    global isPlaying
+    isPlaying = True
+    stateLabel.configure(text="Current state: Playing")
 
+def onManConfigButton():
+    global isPlaying
+    isPlaying = False
+    stateLabel.configure(text="Current state: Manual Configuration")
+
+# Variable that stores state -> 0 means manual input and 1 means playing game
+isPlaying = False
 
 root = Tk()
 
 wideLabel = Label(root, text="Lights Out Game")
-wideLabel.grid(columnspan=5, row=0)
-
-#==============================================================================
-# wideLabel.bind("<Button-1>",paintWhite)
-# wideLabel.bind("<Button-3>",paintBlack)
-#==============================================================================
+wideLabel.grid(columnspan=6, row=0)
 
 rowID = []
 columnID = []
@@ -35,29 +47,29 @@ for i in range(5):
     columnID.append(Label(root, text=str(i+1)))
     columnID[i].grid(row=1, column=i+1)
     
-
 lightGrid = []
 for numRow in range(5):
     for numColumn in range(5):
-        lightGrid.append(Checkbutton(root))
+        i=numRow*5+numColumn
+        lightGrid.append(Checkbutton(root, command=lambda i=i: onPressedButton(i)))
         lightGrid[numRow*5+numColumn].grid(row=numRow+2, column=numColumn+1)
 
 optionsLabel = Label(root, text="Actions:")
-optionsLabel.grid(columnspan=5, row=7)
+optionsLabel.grid(columnspan=6, row=7)
 
-# Change to button
-newConfigurationButton = Label(root, text="New Configuration Button")
-newConfigurationButton.grid(columnspan=5, row=8)
+newConfigurationButton = Button(root, text="New Configuration")
+newConfigurationButton.grid(columnspan=6, row=8)
 
-# Change to button
-manualConfigurationButton = Label(root, text="Manual Configuration Button")
-manualConfigurationButton.grid(columnspan=5, row=9)
+manualConfigurationButton = Button(root, text="Manual Configuration", command=onManConfigButton)
+manualConfigurationButton.grid(columnspan=6, row=9)
 
-# Change to button
-solveButton = Label(root, text="Solve Button")
-solveButton.grid(columnspan=5, row=10)
+playButton = Button(root, text="Play", command=onPlayButton)
+playButton.grid(columnspan=6, row=10)
 
+solveButton = Button(root, text="Solve")
+solveButton.grid(columnspan=6, row=11)
 
-
+stateLabel = Label(root, text="Current state: Manual Configuration")
+stateLabel.grid(columnspan=7, row=12)
 
 root.mainloop()
